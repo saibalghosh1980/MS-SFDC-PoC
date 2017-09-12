161e5c9427bbf3b8abd3c25a8476cc9b232ae5c6
Saibal Ghosh <saibal.ghosh.1980@gmail.com>
2017-09-13 01:17:46 +0530

    NA

---
+++ MS-Location/src/main/java/com/cts/ms/location/controller/ExceptionControllerAdvice.java
@@ -0,0 +1,18 @@
+package com.cts.ms.location.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.cts.ms.location.bo.ExceptionBO;
+
+@ControllerAdvice
+public class ExceptionControllerAdvice {
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ExceptionBO> exceptionHandler(Exception ex) {
+		ExceptionBO error = new ExceptionBO("Please contact your administrator", ex);
+		return new ResponseEntity<ExceptionBO>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+}
