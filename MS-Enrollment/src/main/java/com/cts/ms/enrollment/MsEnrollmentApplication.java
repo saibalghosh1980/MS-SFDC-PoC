38903da3d4ee4bd03d84e2f0ad4ba03a1b0d4f10
Saibal Ghosh <saibal.ghosh.1980@gmail.com>
2017-09-16 00:33:14 +0530

    NA

---
+++ MS-Enrollment/src/main/java/com/cts/ms/enrollment/MsEnrollmentApplication.java
@@ -0,0 +1,12 @@
+package com.cts.ms.enrollment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEnrollmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEnrollmentApplication.class, args);
+	}
+}
---
+++ MS-Enrollment/src/main/java/com/cts/ms/enrollment/util/EnrolllmentStatus.java
@@ -0,0 +1,6 @@
+package com.cts.ms.enrollment.util;
+
+public enum EnrolllmentStatus {
+	PICKEDFORPROCESSING,PROCESSED,REJECTED,UNTOUCHED
+
+}
